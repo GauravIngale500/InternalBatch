3c43795ad606c188267061c40a2e8d2e2b2cc01c
NEXUS <NEXUS@DESKTOP-2AO5O21>
2022-12-07 14:48:07 +0530

    [Abhishek]

--- src/Demo/sample3.java
+++ src/Demo/sample3.java
@@ -4,14 +4,14 @@ public class sample3
 {
   public static void main(String[] args) 
   {
-	String name="Abhishek";
-	int rollno=23;
-	char grade='A';
-	float percent=78.3f;
-	
-	System.out.println(name);
-	System.out.println(rollno);
-	System.out.println(grade);
-	System.out.println(percent);
+		String name="Abhishek";
+		int rollno=43;
+		char grade='A';
+		float percent=78.3f;
+		
+		System.out.println(name);
+		System.out.println(rollno);
+		System.out.println(grade);
+		System.out.println(percent);
   }
 }
