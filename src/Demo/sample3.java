8420b3b4aed9f559ee4bceae8c6f3ba1a8f4370d
NEXUS <NEXUS@DESKTOP-2AO5O21>
2022-12-07 14:37:03 +0530

    [Abhishek]:

---
+++ src/Demo/sample3.java
@@ -0,0 +1,17 @@
+package Demo;
+
+public class sample3 
+{
+  public static void main(String[] args) 
+  {
+	String name="Abhishek";
+	int rollno=23;
+	char grade='A';
+	float percent=78.3f;
+	
+	System.out.println(name);
+	System.out.println(rollno);
+	System.out.println(grade);
+	System.out.println(percent);
+  }
+}
